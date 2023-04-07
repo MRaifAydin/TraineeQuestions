9c0a087e58859140ffd5bf3e4677751b659e4c80
Mert Raif AYDIN <mert5708@gmail.com>
2023-04-07 18:00:52 +0300

    Revert "initial create"
    This reverts commit db328bf30f3f9eee8ea48cd3837099b08da657e2.

---
+++ SumsOfArraysOfAnArray/Program.cs
@@ -0,0 +1,8 @@
+﻿// Calculate individual sum of each array in a given array. And assing each result to another array.
+
+int[] Calculate(int[][] values)
+{
+    
+}
+
+int[][] testData = { new int[] { 1, 2 }, new int[] { 2, 2 }, new int[] { 3, 2 } };
---
+++ SumsOfComplexRows/EntityRow.cs
@@ -0,0 +1,6 @@
+﻿public class EntityRow
+{
+    public int FloorNumber { get; set; }
+
+    public List<string> ClassroomNames { get; set;}
+}
