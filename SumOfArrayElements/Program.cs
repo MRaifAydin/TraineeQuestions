46b3d4614c3b8a5fe9e2556d55cd41e7fc8aa9ef
eminbulbul <113800032+eminbbulbul@users.noreply.github.com>
2023-04-07 18:16:50 +0300

    second task

--- SumOfArrayElements/Program.cs
+++ SumOfArrayElements/Program.cs
@@ -10,7 +10,6 @@ int Calculate(int[] values)
     return result;
 
 
-
 }
 
 int[] testData = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
--- SumsOfArraysOfAnArray/Program.cs
+++ SumsOfArraysOfAnArray/Program.cs
@@ -2,7 +2,18 @@
 
 int[] Calculate(int[][] values)
 {
-    
+    int[] results = new int[values.Length];
+    for (int i = 0; i < values.Length; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < values[i].Length; j++)
+        {
+            sum += values[i][j];
+        }
+        results[i] = sum;
+    }
+    return results;
 }
 
 int[][] testData = { new int[] { 1, 2 }, new int[] { 2, 2 }, new int[] { 3, 2 } };
+int[] results = Calculate(testData);
