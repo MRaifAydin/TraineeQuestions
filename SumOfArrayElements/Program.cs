c8befcbe66c685f31c316573e9cf13bf13bfca9a
eminbulbul <113800032+eminbbulbul@users.noreply.github.com>
2023-04-07 17:42:03 +0300

    first task

--- SumOfArrayElements/Program.cs
+++ SumOfArrayElements/Program.cs
@@ -2,9 +2,12 @@
 
 int Calculate(int[] values)
 {
-
+    int result = 0;
+    for (int i = 0; i < values.Length; i++)
+    {
+        result += values[i];
+    }
+    return result;
 }
-
 int[] testData = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
 Console.WriteLine(Calculate(testData));
