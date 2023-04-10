a2f47ff059deb807d5cf3a74c7f2622f9813d287
emrebulbul <emrebbulbul@gmail.com>
2023-04-10 11:46:24 +0300

    ..

---
+++ SumsOfComplexRows/ResultDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumsOfComplexRows
+{
+    public  class ResultDto
+    {
+        public string SchoolName { get; set; }
+        public int FloorNumber { get; set; }
+
+        public int ClassRoomCount { get; set; }
+    }
+}
